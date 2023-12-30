dd8bf0a47fe42595fac87d3da5c2981545fd7273
naginooi <xxxkmsz@naver.com>
2023-12-30 23:10:55 +0900

    12/30 search Router, Controller 지수

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-import express, { urlencoded } from "express";
-import cors from "cors";
-import MainRouter from "./router/MainRouter.js";
+import express, { urlencoded } from 'express';
+import cors from 'cors';
+import MainRouter from './router/MainRouter.js';
+import SearchRouter from './router/SearchRouter.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cors());
+
+app.use('/search', SearchRouter);
 
 app.listen(9090, () => {
   console.log(`http://localhost:9090 아비브 서버 실행중`);
--- db/database.js
+++ db/database.js
@@ -1,10 +1,10 @@
-import mysql from "mysql2";
+import mysql from 'mysql2';
 
 const pool = mysql.createPool({
-  host: "127.0.0.1",
-  port: "3306",
-  user: "root",
-  password: "1234",
-  database: "abib",
+  host: '127.0.0.1',
+  port: '3306',
+  user: 'root',
+  password: 'qlswlsh198!',
+  database: 'abib',
 });
 export const db = pool.promise();
