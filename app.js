8c4188d185e541d24f4785d33cecf37dc9fab561
DESKTOP-NRC9BD9\User <rkawk2239@naver.com>
2024-01-06 18:48:47 +0900

    01/06 imageUpload complete

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 import express, { urlencoded } from "express";
 import cors from "cors";
 import MainRouter from "./router/MainRouter.js";
+import uploadRouter from "./router/UploadRouter.js";
+import path from "path";
 
 const app = express();
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(cors());
+app.use("/uploads", express.static(path.join("Uploads")));
+
+app.use("/upload", uploadRouter);
 
 app.listen(9090, () => {
   console.log(`http://localhost:9090 아비브 서버 실행중`);
