94fc23a87d7e594b1cf6acf49db836bc1dc328e5
DESKTOP-NRC9BD9\User <rkawk2239@naver.com>
2024-01-08 17:56:57 +0900

    01/06 phone certification

---
+++ controller/MemberController.js
@@ -0,0 +1,18 @@
+// import coolsms from 'coolsms-node-sdk';
+
+// export async function verificationCode(req, res) {
+
+// }
+
+// export async function printTokenResult(phone, token){
+
+//   const mysms = coolsms.default;
+//     const messageService = new mysms("(API KEY 값)","(API 		Secret 값)");
+//     const result = await messageService.sendOne({
+//         to: `${phone}`,
+//          from : '(수신 핸드폰 번호)',
+//          text : `안녕하세요 요청하신 인증번호는 [${token}]입니다.`
+//     })
+
+//     console.log(result);
+// }
