bacc535db7c25c8422f33a743faa9fec7153e37b
whereisjw <j9399977@gmail.com>
2024-01-08 18:35:48 +0900

    01/08 dataconnect jiwon

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import path from "path";
 import uploadRouter from "./router/UploadRouter.js";
-import MainRouter from './router/MainRouter.js';
-import SearchRouter from './router/SearchRouter.js';
-import ProductRouter from './router/ProductRouter.js';
+import MainRouter from "./router/MainRouter.js";
+import SearchRouter from "./router/SearchRouter.js";
+import ProductRouter from "./router/ProductRouter.js";
 
 const app = express();
 
@@ -14,9 +14,9 @@ app.use(cors());
 app.use("/uploads", express.static(path.join("Uploads")));
 app.use("/upload", uploadRouter);
 
-app.use('/product', ProductRouter);
-app.use('/search', SearchRouter);
-
+app.use("/product", ProductRouter);
+app.use("/search", SearchRouter);
+app.use("/", MainRouter);
 app.listen(9090, () => {
   console.log(`http://localhost:9090 아비브 서버 실행중`);
 });
