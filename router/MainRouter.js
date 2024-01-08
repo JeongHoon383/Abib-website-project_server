c9cc7e48e2d865f3ae6c1e900ec30619610e4d0a
whereisjw <j9399977@gmail.com>
2024-01-08 18:35:48 +0900

    01/08 dataconnect jiwon

--- router/MainRouter.js
+++ router/MainRouter.js
@@ -1,5 +1,9 @@
 import express from "express";
+import { getProducts, getRecommend } from "../controller/MainController.js";
 
 const router = express.Router();
+
+router.get("/", getProducts);
+router.get("/", getRecommend);
 
 export default router;
