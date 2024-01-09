90806093c4905065d842f441d3bfdd8213d290fe
whereisjw <j9399977@gmail.com>
2024-01-09 17:26:38 +0900

    01/09 main data jiwon

--- repository/MainRepository.js
+++ repository/MainRepository.js
@@ -1,12 +1,5 @@
 import { db } from "../db/Database.js";
 
 export async function getProducts() {
-  return db
-    .execute(`SELECT * FROM product WHERE priceSales > 1`, [])
-    .then((result) => result);
-}
-export async function getRecommend() {
-  return db
-    .execute(`SELECT * FROM product WHERE title like '%부활초%'`, [])
-    .then((result) => result);
+  return db.execute(`SELECT * FROM product`, []).then((result) => result);
 }
