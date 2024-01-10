1d006a20d9b548cd9ea932e2cca1cf652bd94397
DESKTOP-NRC9BD9\User <rkawk2239@naver.com>
2024-01-11 03:06:41 +0900

    01/10 general signup complete

--- controller/MemberController.js
+++ controller/MemberController.js
@@ -1,8 +1,17 @@
 import coolsms from "coolsms-node-sdk";
+import * as repository from "../repository/MemberRepository.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export async function getIsIdAvailable(req, res) {
+  const id = req.params.idValue;
+  const result = await repository.getIsIdAvailable(id);
+  res.json(result);
+}
 
 export async function getCertificationCode(req, res) {
   const { phoneValue } = req.body;
-  const getCertificationCode = Math.floor(Math.random() * 1000000);
+  const certificationCode = Math.floor(Math.random() * 1000000);
   const mysms = coolsms.default;
   const messageService = new mysms(
     "NCSN3GO1YUSM6DIT",
@@ -12,12 +21,40 @@ export async function getCertificationCode(req, res) {
   const result = await messageService.sendOne({
     to: `${phoneValue}`,
     from: "01085486516",
-    text: `아비브 인증번호는 [${getCertificationCode}]입니다.`,
+    text: `아비브 인증번호는 [${certificationCode}]입니다.`,
   });
 
   if (result.statusCode === "2000") {
-    res.json(getCertificationCode);
+    res.json(certificationCode);
   } else {
     res.json("faillure");
   }
+}
+
+export async function insertMember(req, res) {
+  const {
+    id,
+    password,
+    name,
+    phone,
+    birthdate,
+    email,
+    postalcode,
+    address1,
+    address2,
+  } = req.body;
+  const hashPass = bcrypt.hashSync(password, 10);
+  const formData = [
+    id,
+    hashPass,
+    name,
+    phone,
+    email,
+    birthdate,
+    postalcode,
+    address1,
+    address2,
+  ];
+  const result = await repository.insertMember(formData);
+  res.json(result);
 }
