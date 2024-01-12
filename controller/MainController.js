da79e2c24fd097ae6ea80a8edae6d2511964f9c3
whereisjw <j9399977@gmail.com>
2024-01-12 17:41:10 +0900

    1/12 qna insert jiwon

--- controller/MainController.js
+++ controller/MainController.js
@@ -3,3 +3,12 @@ export async function getProducts(req, res) {
   let result = await MainRepository.getProducts();
   res.json(result[0]);
 }
+
+export async function insertQna(req, res) {
+  const { qTitle, qContent, mid } = req.body;
+  console.log(qTitle, qContent, mid);
+  let result = await MainRepository.insertQna(qTitle, qContent, mid);
+  if (result == "ok") {
+    res.json({ ok: true });
+  }
+}
