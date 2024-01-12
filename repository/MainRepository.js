83c003f52bef49f13b3199abeae2b5d3aee16470
whereisjw <j9399977@gmail.com>
2024-01-12 17:41:10 +0900

    1/12 qna insert jiwon

--- repository/MainRepository.js
+++ repository/MainRepository.js
@@ -3,3 +3,12 @@ import { db } from "../db/Database.js";
 export async function getProducts() {
   return db.execute(`SELECT * FROM product`, []).then((result) => result);
 }
+
+export async function insertQna(mid, qTitle, qContent) {
+  return db
+    .execute(
+      `INSERT INTO qna (mid,qTitle,qContent,qDate) values(?,?,?,curdate())`,
+      [mid, qTitle, qContent]
+    )
+    .then((result) => "ok");
+}
