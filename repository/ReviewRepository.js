6a167beba40afc7e6bef93e4e04263c87bf890c7
naginooi <xxxkmsz@naver.com>
2024-01-14 22:15:27 +0900

    01/14 review sort jisu

--- repository/ReviewRepository.js
+++ repository/ReviewRepository.js
@@ -1,11 +1,11 @@
 import { db } from '../db/Database.js';
 
 export async function saveReview(pid, mid, point, content, rcover) {
-  const sql = 'insert into review (pid, mid, point, content, rdate, rcover) values(?, ?, ?, ?, DATE(sysdate()), ?)';
+  const sql = 'insert into review (pid, mid, point, content, rdate, rcover) values(?, ?, ?, ?, now(), ?)';
   return db.execute(sql, [pid, mid, point, content, rcover || null]).then((result) => 'success');
 }
 
 export async function getReview(pid) {
-  const sql = 'select * from review where pid = ?';
+  const sql = 'select * from review where pid = ? order by rdate desc';
   return db.execute(sql, [pid]).then((result) => result[0]);
 }
