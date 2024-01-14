92fa9667b11db35dc13fd8129b76d5aac24ca7bd
DESKTOP-NRC9BD9\User <rkawk2239@naver.com>
2024-01-14 21:43:59 +0900

    01/14 order

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -1,5 +1,11 @@
 import * as repository from "../repository/OrderRepository.js";
 
+export async function order(req, res) {
+  const orderData = req.body;
+  const result = await repository.order(orderData);
+  console.log(result);
+}
+
 export async function getOrdererInfo(req, res) {
   const memberId = req.params.memberId;
   const result = await repository.getOrdererInfo(memberId);
--- router/OrderRouter.js
+++ router/OrderRouter.js
@@ -3,6 +3,7 @@ import * as controller from "../controller/OrderController.js";
 
 const router = express.Router();
 
+router.post("/", controller.order);
 router.get("/getOrdererInfo/:memberId", controller.getOrdererInfo);
 
 export default router;
