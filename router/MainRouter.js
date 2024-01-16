f5dd999e3f370f339c488d73f9d24e781ab00e8a
whereisjw <j9399977@gmail.com>
2024-01-16 14:07:20 +0900

    01/16 for ppt jiwon

--- router/MainRouter.js
+++ router/MainRouter.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { getProducts, insertQna } from "../controller/MainController.js";
+import {
+  getOrderList,
+  getProducts,
+  insertQna,
+} from "../controller/MainController.js";
 
 const router = express.Router();
 
 router.get("/", getProducts);
 
 router.post("/", insertQna);
+
+router.get("/orderlist/:id", getOrderList);
 
 export default router;
